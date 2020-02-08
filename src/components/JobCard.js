033b3798fec3d87dfc7cc2b0e3f9b4599a4e28c6
ncooney94 <nick.cooney@gmail.com>
2020-02-08 16:57:27 +1100

    g

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -2,27 +2,90 @@ import React, {useState, useEffect} from 'react';
 
 import axios from 'axios';
 function JobCard(props) {
-  console.log(props);
   
-
-  // const [userId, setUserId] = useState(props.userId)
   const [jobs, setJobs] = useState(null)
-
+  const [job, setJob] = useState(null)
+  const userId = props.workerId
   useEffect(() => {
-    const userId = props.workerId
+    
     axios.get(`https://test.swipejobs.com/api/worker/${userId}/matches`).then(res => {
-        // console.log(user, 'hey');
-        setJobs(res.data)
-        
+        setJobs(res.data)   
     })
   }, []);
+
+  const acceptJob = () => {
+    axios.post(`https://test.swipejobs.com/api/worker/${userId}/job/${props.jobId}/accept`).then(res => {
+      console.log(res);
+      
+    })
+  }  
+  const rejectJob = () => {
+    axios.post(`https://test.swipejobs.com/api/worker/${userId}/job/${props.jobId}/reject`).then(res => {
+      console.log(res);
+      
+    })
+  }
 
 if (jobs) {
   var jobDetails = jobs.map( job => {
     if (job.jobId === props.jobId) {
-      console.log('yup');
+      let req = 'nothing'
+      if (job.requirements) {
+        req = '-' + job.requirements
+      }
       
-      return <div><h2>yup</h2></div>
+      return <div>
+              <div className="hero">
+                <img className="hero ui fluid image" src={job.jobTitle.imageUrl} />
+              </div>
+              <div className="block ">
+                <h3>{job.jobTitle.name}</h3>
+                <h4>{job.company.name}</h4>
+              </div>
+              <div className="block  highlight">
+                <div className="part">
+                    <p>Distance</p>
+                    <h4>{job.milesToTravel}miles</h4>
+                </div>
+                <div className="part2">
+                    <p>Hourly Rate</p>
+                    <h4>{job.wagePerHourInCents/100}</h4>
+                </div>
+              </div>
+              <div className="block ">            
+                <i className="calendar alternate icon"></i>
+                <span className="flex">
+                <p>Shift Dates</p>
+                <h4>{job.shifts[0].startDate}</h4>
+                <h4>{job.shifts[0].endDate}</h4>
+                </span>
+              </div>
+              <hr/>
+              <div className="block ">                
+                <i className="map marker icon alternate"></i>
+                <span className="flex">
+                  <p>Location</p>
+                  <h4>{job.company.address.formattedAddress}</h4>
+                  <p>{job.milesToTravel} miles from your job search location</p>
+                </span>
+                <i className="chevron right icon"></i>                
+              </div>
+              <hr/>
+              <div className="block ">            
+                <span className="flex">
+                <p>Requirements</p>
+                <p>{req}</p>
+                </span>     
+              </div>
+              <hr/>
+              <div className="block ">         
+                <i className="user circle icon"></i>
+                <span className="flex">
+                <p>Report to</p>
+                <h4>{job.company.reportTo.name} {job.company.reportTo.phone}</h4>
+                </span>              
+              </div>
+            </div>
     }
   })
 }
@@ -31,71 +94,16 @@ if (jobs) {
     
     return(
         <div className="job ">
-            {/* <div className="hero"> */}
-            {/* <img className="hero ui fluid image" src={data[0].jobTitle.imageUrl}></img> */}
-            {/* </div> */}
-            
-            <div className="block ">
-                {/* <h3>{data[0].jobTitle.name}</h3> */}
-                {/* <h4>{data[0].company.name}</h4> */}
-                {jobDetails}
-                
-            </div>
-            <div className="block  highlight">
-                <div className="part">
-                    <p>Distance</p>
-                    {/* <h4>{data[0].milesToTravel}miles</h4> */}
-                </div>
-                <div className="part2">
-                    <p>Hourly Rate</p>
-                    {/* <h4>${data[0].wagePerHourInCents/100}</h4> */}
-                </div>
-                
-            </div>
-            <div className="block ">
-                
-                <i className="calendar alternate icon"></i>
-                <span className="flex">
-                <p>Shift Dates</p>
-                {/* <h4>{data[0].shifts[0].startDate}</h4>
-                <h4>{data[0].shifts[0].endDate}</h4> */}
-                </span>
-                
-            </div>
-            <hr/>
-            <div className="block ">
-                
-                <i className="map marker icon alternate"></i>
-                <span className="flex">
-                <p>Location</p>
-                {/* <h4>{data[0].company.address.formattedAddress}</h4>
-                <p>{data[0].milesToTravel} miles from your job search location</p> */}
-                </span>
-                <i className="chevron right icon"></i>
-                
-            </div>
-            <hr/>
-            <div className="block ">
-                
-                <span className="flex">
-                <p>Requirements</p>
-                </span>
-                
-            </div>
-            <hr/>
-            <div className="block ">
-                
-                <i className="user circle icon"></i>
-                <span className="flex">
-                <h4>Report to</h4>
-                {/* <h4>{data[0].company.reportTo.name} {data[0].company.reportTo.phone}</h4> */}
-                </span>
-                
-            </div>
+
+            {jobDetails}
+
+
+
+
             {/* <div className="block "> */}
             <div className="block actions">
-                <button className="big ui grey basic button">No Thanks</button>
-                <button className="big ui secondary button">I'll Take It</button>
+                <button onClick={rejectJob} className="big ui grey basic button">No Thanks</button>
+                <button onClick={acceptJob} className="big ui secondary button">I'll Take It</button>
             </div>
             {/* </div> */}
         </div>
