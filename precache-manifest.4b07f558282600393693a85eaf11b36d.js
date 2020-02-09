3e97fbd2deea2520e54561a3f6c8633a58e604fc
ncooney94 <nick.cooney@gmail.com>
2020-02-10 01:56:50 +1100

    Updates

---
+++ precache-manifest.4b07f558282600393693a85eaf11b36d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c755d8af46ed41b64fcca654cf6445b0",
+    "url": "/swipejobs/index.html"
+  },
+  {
+    "revision": "9138e442f8f62f63d39c",
+    "url": "/swipejobs/static/css/main.012f6cc2.chunk.css"
+  },
+  {
+    "revision": "2bac7f894f1b6235d30d",
+    "url": "/swipejobs/static/js/2.995e8192.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/swipejobs/static/js/2.995e8192.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9138e442f8f62f63d39c",
+    "url": "/swipejobs/static/js/main.bc443403.chunk.js"
+  },
+  {
+    "revision": "d904b43a2b8b06d76535",
+    "url": "/swipejobs/static/js/runtime-main.dcf072dd.js"
+  }
+]);
